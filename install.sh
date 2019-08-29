751702c8858b61616fd95af20ec89a609a1242e9
Gustavo Verzola <verzola@gmail.com>
2019-08-28 21:05:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-if [ ! -d "~/projects" ]; then
+if [ ! -d ~/projects ]; then
   mkdir ~/projects
 fi
-git clone https://github.com/verzola/aliases.git ~/projects/aliases
+
+if [ ! -d ~/projects/aliases ]; then
+    git clone https://github.com/verzola/aliases.git ~/projects/aliases
+else
+    cd ~/projects/aliases
+    git pull origin master
+fi
